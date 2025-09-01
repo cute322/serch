ad95563b62e2a9d7940d8dc0c742aa6fd5d4e1ed
cute322 <dalilacherifslimane1@gmail.com>
2025-09-01 21:32:31 +0200

    Update firebase.ts

--- TaskPilot/client/src/lib/firebase.ts
+++ TaskPilot/client/src/lib/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCH4AxyOpQCNyPFcux9lfgr-TeYUx2VMFU",
-  authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID || "academic-771ec"}.firebaseapp.com`,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "academic-771ec",
-  storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID || "academic-771ec"}.firebasestorage.app`,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "577614362103",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:577614362103:web:b398dcdd2047f3f393aaf2",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-4QD9R9J80Y"
+  apiKey: "AIzaSyCH4AxyOpQCNyPFcux9lfgr-TeYUx2VMFU",
+  authDomain: "academic-771ec.firebaseapp.com",
+  projectId: "academic-771ec",
+  storageBucket: "academic-771ec.firebasestorage.app",
+  messagingSenderId: "577614362103",
+  appId: "1:577614362103:web:b398dcdd2047f3f393aaf2",
+  measurementId: "G-4QD9R9J80Y"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
